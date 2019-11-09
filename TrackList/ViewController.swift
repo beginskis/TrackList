e72f9d6ccb3a0a6091b0929572cdf93ed719cbae
Dmitrijs Beginskis <beginskis@gmail.com>
2019-11-09 10:05:37 +0200

    Initial Commit

---
+++ TrackList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrackList
+//
+//  Created by Dmitrijs Beginskis on 09/11/2019.
+//  Copyright © 2019 Dmitrijs Beginskis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
